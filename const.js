e665fc6ecdc396d26a7e22b9a459a9b2d4a0a21a
Thomas Liu <liuxk99@gmail.com>
2023-04-07 18:42:29 +0800

    <CM>: init project files.

---
+++ const.js
@@ -0,0 +1,15 @@
+console.log("=> typeof");
+const a = 3;
+console.log("a: " + typeof(a));
+
+const b = 3.14
+console.log("b: " + typeof(b));
+
+const c = "Hello JS!";
+console.log("c: " + typeof(c));
+
+const d = false;
+console.log("d: " + typeof(d));
+
+const e = 4 > 3;
+console.log("e: " + typeof(e));
---
+++ liters.js
@@ -0,0 +1,28 @@
+console.log("=> liter values");
+console.log(3);
+console.log(3.14);
+console.log(false);
+console.log(true);
+console.log("Hello JS!");
+
+console.log("=> number operation");
+console.log(4 + 3);
+console.log(4 - 3);
+console.log(4 * 3);
+console.log(4 / 3);
+console.log(12 / 3);
+console.log(4 % 3);
+
+console.log("=> string operation");
+console.log("Hello" + " " + "JS!");
+
+console.log("=> comparation operation");
+console.log(4 > 3);
+console.log(4 == 3);
+console.log(3 === 3);
+console.log(4 < 3);
+
+console.log("=> boolean operation");
+console.log(4 < 3 && 2 > 1);
+console.log(4 < 3 || 2 > 1);
+console.log(!(4 < 3));
